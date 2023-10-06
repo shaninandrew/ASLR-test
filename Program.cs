72f840c203b551a17bd07262f118bb702bcb3627
hedgestandart@gmail.com <hedgestandart@gmail.com>
2023-10-06 12:31:59 +0300

    Fix

--- Program.cs
+++ Program.cs
@@ -1,25 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(">");
-
-//входное значение
-int i = 0;
-if (args.Length > 0)
+try
 {
+    Console.WriteLine(">");
+
     //входное значение
-    i = int.Parse(args[0]) + 1;
+    int i = 0;
+    if (args.Length > 0)
+    {
+        //входное значение
+        i = int.Parse(args[0]) + 1;
+    }
+
+    AppDomain domain = AppDomain.CurrentDomain;
+
+
+    //AppDomain Another = AppDomain.CreateDomain("new_dom");
+    //сама программа
+    string[] parms = new string[1] { i.ToString() };
+    Console.WriteLine($"Запуск - передано {i}");
+
+    if (i < 1936)
+    {
+        try
+        {
+            Console.WriteLine($" *  Память {GC.GetTotalAllocatedBytes() / 1000} кб  - общий объем {GC.GetTotalMemory(false) / 1000} Кб");
+            domain.ExecuteAssembly("ASLR test.dll", parms);
+            Console.WriteLine($"===> {i} ");
+        }
+        catch (StackOverflowException ex)
+        { }
+    }
 }
-
-AppDomain domain = AppDomain.CurrentDomain;
-
-
-//AppDomain Another = AppDomain.CreateDomain("new_dom");
-//сама программа
-string[] parms = new string[1] { i.ToString() };
-Console.WriteLine($"Запуск - передано {i}");
-
-if (i < 200)
+catch (StackOverflowException ex)
 {
-    Console.WriteLine($" *  Память {GC.GetTotalAllocatedBytes() / 1000} кб  - общий объем {GC.GetTotalMemory(false)/1000} Кб" );
-    domain.ExecuteAssembly("ASLR test.dll", parms);
+    Console.WriteLine("Все... стоп!");
 }
-Console.WriteLine($"===> {i} ");
+
