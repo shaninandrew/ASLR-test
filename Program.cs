5c3cdc6bd8e53ab4e63d2c95a25ead91edc2b22e
hedgestandart@gmail.com <hedgestandart@gmail.com>
2023-10-06 12:19:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(">");
+
+//входное значение
+int i = 0;
+if (args.Length > 0)
+{
+    //входное значение
+    i = int.Parse(args[0]) + 1;
+}
+
+AppDomain domain = AppDomain.CurrentDomain;
+
+
+//AppDomain Another = AppDomain.CreateDomain("new_dom");
+//сама программа
+string[] parms = new string[1] { i.ToString() };
+Console.WriteLine($"Запуск - передано {i}");
+
+if (i < 200)
+{
+    Console.WriteLine($" *  Память {GC.GetTotalAllocatedBytes() / 1000} кб  - общий объем {GC.GetTotalMemory(false)/1000} Кб" );
+    domain.ExecuteAssembly("ASLR test.dll", parms);
+}
+Console.WriteLine($"===> {i} ");
